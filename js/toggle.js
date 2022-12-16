747e16ab287df88ee8bfb5d2f25ed5bf5e5a8b03
Sheldon <sheldonaldridge@yahoo.com>
2022-12-16 23:45:42 +0200

    click event

---
+++ js/toggle.js
@@ -0,0 +1,7 @@
+let contact = document.querySelector(".contact")
+let email = document.querySelector(".email")
+
+contact.addEventListener("click",() =>{
+    email.classList.toggle("dispaly")
+})
+
