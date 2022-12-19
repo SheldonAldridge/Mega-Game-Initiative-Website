c77ee5731cfd5c6265406f934728eb0cfb6f5c15
CentralStation247 <sheldon@centralstation247.co.za>
2022-12-19 21:22:21 +0200

    update code

--- js/toggle.js
+++
@@ -1,8 +0,0 @@
-let contact = document.querySelector(".contact")
-let email = document.querySelector(".email")
-
-contact.addEventListener("click",(e) =>{
-    e.preventDefault();
-    email.classList.toggle("dispaly");
-})
-
